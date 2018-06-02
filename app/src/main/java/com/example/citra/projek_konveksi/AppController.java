c0fc82399a8feba58fa8f4d918e5bdc229c79177
DESKTOP-9KODVNA\Citra <citransa8@gmail.com>
2018-06-02 15:32:53 +0700

    xml

---
+++ app/src/main/java/com/example/citra/projek_konveksi/AppController.java
@@ -0,0 +1,8 @@
+package com.example.citra.projek_konveksi;
+
+/**
+ * Created by Citra on 02/06/2018.
+ */
+
+public class AppController {
+}
---
+++ app/src/main/java/com/example/citra/projek_konveksi/konveksi.java
@@ -0,0 +1,49 @@
+package com.example.citra.projek_konveksi;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class konveksi extends AppCompatActivity {
+
+    private DrawerLayout draw;
+    private ActionBarDrawerToggle toggle;
+
+    private Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konveksi);
+
+        setTitle("Konveksi");
+
+        //buat action barnya
+        toolbar= findViewById(R.id.nav_action);
+        setSupportActionBar(toolbar);
+
+        //membuat sidebar -start-
+        draw = findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, draw,R.string.open,R.string.close);
+
+        draw.addDrawerListener(toggle);
+        toggle.syncState();
+        //-end-
+
+        //membuat icon sidebar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //agar icon dipencet keluar sidebar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
